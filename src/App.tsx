48d8e91ad36767a131f2b4f2e355cd293973e3c9
moonsmile-dev <kakavip.198.kaka@gmail.com>
2021-02-08 15:29:18 +0700

    Manage user report modules.

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,15 @@ import { UserAccountList } from './modules/user_accounts/user_account_list';
 import { userAccountResource } from './modules/user_accounts';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import ReportIcon from '@material-ui/icons/Report';
 import { accountVerifyResource } from './modules/account_verifies';
+import { accountReportResource } from './modules/account_reports';
 
 const dataProvider = drfProvider('http://127.0.0.1:8000/api');
 const App = () => <Admin dataProvider={dataProvider}>
     <Resource name="user_accounts"  {...userAccountResource} icon={AccountBoxIcon} />
     <Resource name="account_verifies"  {...accountVerifyResource} icon={VerifiedUserIcon} />
+    <Resource name="account_reports"  {...accountReportResource} icon={ReportIcon} />
 </Admin>;
 
 
